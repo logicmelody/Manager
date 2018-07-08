fe7e68341f84be2fabfb4215597b6f64803e8f13
Danny Lin <logicmelody@gmail.com>
2018-07-08 21:32:59 +0800

    Complete user login

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,26 +3,51 @@ import {
 	EMAIL_CHANGED,
 	PASSWORD_CHANGED,
 	LOGIN_USER_SUCCESS,
+	LOGIN_USER_FAIL,
+	LOGIN_USER_ING,
 } from './types';
 
 export const emailChanged = (text) => ({
 	type: EMAIL_CHANGED,
-	payload: text
+	payload: text,
 });
 
 export const passwordChanged = (text) => ({
 	type: PASSWORD_CHANGED,
-	payload: text
+	payload: text,
 });
 
 export const loginUser = ({ email, password }) => {
 	return (dispatch) => {
+		loginUserIng(dispatch);
+
 		firebase.auth().signInWithEmailAndPassword(email, password)
-			.then(user => {
-				dispatch({
-					type: LOGIN_USER_SUCCESS,
-					payload: user
-				})
+			.then(user => loginUserSuccess(dispatch, user))
+			.catch((error) => {
+				console.log(error);
+
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.then(user => loginUserSuccess(dispatch, user))
+					.catch(() => loginUserFail(dispatch));
 			});
 	};
 };
+
+function loginUserSuccess(dispatch, user) {
+	dispatch({
+		type: LOGIN_USER_SUCCESS,
+		payload: user,
+	});
+}
+
+function loginUserFail(dispatch) {
+	dispatch({
+		type: LOGIN_USER_FAIL,
+	});
+}
+
+function loginUserIng(dispatch) {
+	dispatch({
+		type: LOGIN_USER_ING,
+	});
+}
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+} from 'react-native';
 import { connect } from 'react-redux';
-import { Card, CardSection, Input, Button } from './common';
-import { emailChanged, passwordChanged, loginUser } from '../actions';
+import {
+	Card,
+	CardSection,
+	Input,
+	Button,
+	Spinner,
+} from './common';
+import {
+	emailChanged,
+	passwordChanged,
+	loginUser,
+} from '../actions';
 
 class LoginForm extends Component {
 	_onEmailChange(text) {
@@ -14,6 +29,24 @@ class LoginForm extends Component {
 		const { email, password } = this.props;
 
 		this.props.loginUser({ email, password });
+	}
+	_renderError() {
+		const { error } = this.props;
+
+		if (error) {
+			return (
+				<View style={{ backgroundColor: 'white' }}>
+					<Text style={styles.errorTextStyle}>{error}</Text>
+				</View>
+			);
+		}
+	}
+	_renderButton() {
+		return this.props.loading ?
+			<Spinner size={'large'} /> :
+			<Button onPress={this._onButtonPress.bind(this)}>
+				Login
+		  	</Button>;
 	}
 	render() {
 		return (
@@ -37,19 +70,28 @@ class LoginForm extends Component {
 					/>
 				</CardSection>
 
+				{this._renderError()}
+
 				<CardSection>
-					<Button onPress={this._onButtonPress.bind(this)}>
-						Login
-          			</Button>
+					{this._renderButton()}
 				</CardSection>
 			</Card>
 		);
 	}
 }
 
-const mapStateToProps = state => ({
-	email: state.auth.email,
-	password: state.auth.password
+const styles = StyleSheet.create({
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red',
+	},
 });
+
+const mapStateToProps = state => {
+	const { email, password, error, loading } = state.auth;
+
+	return { email, password, error, loading };
+};
 
 export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm);
